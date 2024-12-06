0a956a765fea339e0c16cb9f753ec8d13b6adec8
Aliza Bataluna <alizabataluna1@gmail.com>
2024-12-06 21:03:13 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+export default function App() {
+  return (
+    <>
+    <div><h1 className="text-5xl font-bold underline text-center">
+        Hello world!
+      </h1>
+      <button className="rounded-lg border border-transparent py-2 px-4 text-white bg-gray-800 hover:border-blue-500">
+        Click Me
+      </button></div>
+      
+    </>
+  )
+}
